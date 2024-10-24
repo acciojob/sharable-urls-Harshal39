7a9bb72ac959ca7dcc39710d9efd75bfae69f61a
Harshal Bhute <harshalbhute39@gmail.com>
2024-10-24 14:38:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+let button = document.getElementById('button');
+button.addEventListener(click,() => {
+	const baseUrl = "https://localhost:8080/";
+    const name = document.getElementById('name').value;
+    const year = document.getElementById('year').value;
+    let queryString = baseUrl;
+
+    if (name) {
+        queryString += `?name=${encodeURIComponent(name)}`;
+    }
+    if (year) {
+        if (name) {
+            queryString += `&year=${encodeURIComponent(year)}`;
+        } else {
+            queryString += `?year=${encodeURIComponent(year)}`;
+        }
+    }
+
+    document.getElementById('url').innerText = queryString;
+});
